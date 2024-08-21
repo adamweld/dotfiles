7cd7fb86ba6cea7a7f11a0b0c32327a2900c672e
Adam Weld <adam.weld@gmail.com>
2024-08-21 15:23:53 -0700

    updates for windows install

--- install.ps1
+++ install.ps1
@@ -2,20 +2,26 @@ $ErrorActionPreference = "Stop"
 
 $CONFIG = "adamweld.conf.yaml"
 $DOTBOT_DIR = "dotbot"
-
 $DOTBOT_BIN = "bin/dotbot"
+$PLUGINS_DIR = "plugins"
 $BASEDIR = $PSScriptRoot
-
 Set-Location $BASEDIR
-git -C $DOTBOT_DIR submodule sync --quiet --recursive
-git submodule update --init --recursive $DOTBOT_DIR
 
+$DOTBOT_CMD =   $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) 
+$PLUGINS_CMD = @(Get-ChildItem -Directory $PLUGINS_DIR | ForEach-Object { "--plugin-dir", "$PLUGINS_DIR/$($_.Name)" })
+
+Write-Output "Dotfiles Repo Status:"
+git status
+git submodule sync --quiet --recursive
+git submodule update --init --recursive $BASEDIR
+
+Write-Output "Executing Dotbot:"
 foreach ($PYTHON in ('python', 'python3', 'python2')) {
     # Python redirects to Microsoft Store in Windows 10 when not installed
     if (& { $ErrorActionPreference = "SilentlyContinue"
             ![string]::IsNullOrEmpty((&$PYTHON -V))
             $ErrorActionPreference = "Stop" }) {
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
+        & $PYTHON $DOTBOT_CMD --base-directory $BASEDIR --config-file $CONFIG $PLUGINS_CMD $Args
         return
     }
 }
